9e4acf91dd86e4182daf1724d2a34026538a7678
alok.rahul <alok.rahul.devops@gmail.com>
2024-11-03 12:00:15 +0100

    Get the Expense from the User

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,8 +1,19 @@
+from expense import Expense
+
+
 def main():
     print(f"💶Running Expense Tracker 🧾!")
-
     # Get User Input for the Expense
-def get_user_input():
+    expense = get_user_expense()
+    print(expense)
+
+    #Write their expense to a file
+    save_expense_to_file()
+
+    #Read file and summarize the expense
+    summarize_expense()
+    
+def get_user_expense():
     print(f"💶Obtaining the User's Input🧾!")
     expense_name = input("Enter the Expense Name : ")
     expense_amount = float(input("Enter the Expense Amount : "))
@@ -13,36 +24,36 @@ def get_user_input():
         "🍽️ Food",
         "🥳 Fun",
         "✍🏼 Misc"
-    ]
-
-    # We will use while becuase it will keep getting the input until user provides the valid input from the mentioned list
-
-    while True:
+    ]    
+    while True:          # We will use while becuase it will keep getting the input
         print("Select a category: ")
         for i, category_name in enumerate(expense_categories):
             print(f"  {i+1}. {category_name} ")
-        
         value_range = f"[1- {len(expense_categories)}]"
-        selected_categ = int(input(f"Enter the category number {value_range}")) - 1
+        category_index = int(input(f"Enter the category number {value_range}")) - 1
 
-        if selected_categ in range(len(expense_categories)):
-            break
+        if category_index in range(len(expense_categories)):
+            selected_category = expense_categories[category_index]
+            new_expense = Expense(
+                name=expense_name , category=selected_category, amount=expense_amount
+            )
+            return new_expense
         else:
             print("Invalid Input, please try again ! ")
 
 
-    #Write their expense for to a file
+
+#Write their expense for to a file
 def save_expense_to_file():
     print(f"💶Saving the expense to a file🧾!")
 
-    # Read file and summarize the expense
+
+
+# Read file and summarize the expense
 def summarize_expense():
     print(f"💶summarize User Expense🧾!")
 
 if __name__ == "__main__":
     main()
-    get_user_input()
-    save_expense_to_file()
-    summarize_expense()
 
 
